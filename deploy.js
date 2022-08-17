60a92436f2b519eda2c6b2a78e42ce5fc43b42c6
Carlos Mario <carlosmario7lg@gmail.com>
2022-08-17 16:27:41 -0500

    deploy on local blockchain ganache with ethers.js

---
+++ deploy.js
@@ -0,0 +1,29 @@
+const ethers = require("ethers");
+const fs = require("fs-extra");
+
+async function main() {
+  // http://127.0.0.1:7545
+  const provider = new ethers.providers.JsonRpcProvider(
+    "http://127.0.0.1:7545"
+  );
+  const wallet = new ethers.Wallet(
+    "78708efdd4fa7c33f664daa7fc812e19e2009cd37b3bb97fc8c5f561d695a7d2",
+    provider
+  );
+  const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
+  const binary = fs.readFileSync(
+    "./SimpleStorage_sol_SimpleStorage.bin",
+    "utf8"
+  );
+  const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
+  console.log("deploying, please wait...");
+  const contract = await contractFactory.deploy();
+  console.log(contract);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
